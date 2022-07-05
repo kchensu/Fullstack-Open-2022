cb18880440ed0c50d01e0ee9de2f7374ff139364
kevin <kchensu@sfu.ca>
2022-07-05 14:15:35 -0700

    init

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.post("/api/notes", (request, response) => {
 })
 
 app.use(unknownEndpoint)
-const PORT = 3001 || process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
